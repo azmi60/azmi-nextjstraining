44598dfbc0ba86154bc10c13c0b1526f4e8c9a25
azmi60 <mohammad.azmi@sirclo.com>
2022-11-02 15:11:39 +0700

    Add progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
-import Layout from "@/components/Layout"
+import Layout from "@/components/Layout";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <NextNProgress />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
